80c7aaa84e6d76fcc8972f23011c1547ed6fe81e
jarod94 <hi@amorekazonsou.com>
2021-09-26 07:31:33 +0100

    step

--- __spec/factories/labellings.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :labelling do
-    label_id { 2 }
-    task_id { 2 }
-  end
-end
--- __spec/factories/labels.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :label do
-    title { "Label 1" }
-  end
-
-  factory :label2, class: Label do
-  	title { "Label 2" }
-  end
-end
